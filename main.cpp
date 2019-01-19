7b0f5436062004cdcdc6cd2649356b12a876015e
Richard Cook <rcook@rcook.org>
2019-01-18 21:08:39 -0800

    Check all results consistently

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@
 using Renderer = std::unique_ptr<SDL_Renderer, void(*)(SDL_Renderer*)>;
 using Window = std::unique_ptr<SDL_Window, void(*)(SDL_Window*)>;
 
+template<typename T>
+inline void check_sdl_resource(const T& resource) {
+    if (!resource) {
+        throw std::runtime_error(SDL_GetError());
+    }
+}
+
+inline void check_sdl_status(int status) {
+    if (status != 0) {
+        throw std::runtime_error(SDL_GetError());
+    }
+}
+
 int main(int argc, char *argv[]) {
     Window window(SDL_CreateWindow(
         "Hello World",
@@ -13,28 +26,24 @@ int main(int argc, char *argv[]) {
         480,
         SDL_WINDOW_SHOWN),
         SDL_DestroyWindow);
-    if (!window) {
-        throw std::runtime_error(SDL_GetError());
-    }
+    check_sdl_resource(window);
 
     Renderer renderer(SDL_CreateRenderer(
         window.get(),
         -1,
         SDL_RENDERER_ACCELERATED),
         SDL_DestroyRenderer);
-   if (!renderer) {
-        throw std::runtime_error(SDL_GetError());
-    }
+    check_sdl_resource(renderer);
 
     // Clear background colour to red
-    SDL_SetRenderDrawColor(renderer.get(), 255, 0, 0, 255);
-    SDL_RenderClear(renderer.get());
+    check_sdl_status(SDL_SetRenderDrawColor(renderer.get(), 255, 0, 0, 255));
+    check_sdl_status(SDL_RenderClear(renderer.get()));
 
     // Set render colour to blue
-    SDL_SetRenderDrawColor(renderer.get(), 0, 0, 255, 255);
+    check_sdl_status(SDL_SetRenderDrawColor(renderer.get(), 0, 0, 255, 255));
 
     SDL_Rect r{ 50, 50, 50, 50 };
-    SDL_RenderFillRect(renderer.get(), &r);
+    check_sdl_status(SDL_RenderFillRect(renderer.get(), &r));
     SDL_RenderPresent(renderer.get());
 
     SDL_Delay(1000);
