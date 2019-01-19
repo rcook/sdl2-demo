c81fa3429508fa31e7f18d6f837678b64eb46567
Richard Cook <rcook@rcook.org>
2019-01-19 06:26:51 -0800

    Draw lines

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <algorithm>
 #include <memory>
 
 using Renderer = std::unique_ptr<SDL_Renderer, void(*)(SDL_Renderer*)>;
@@ -17,9 +18,20 @@ inline void check_sdl_status(int status) {
     }
 }
 
+inline void check_sdl_bool(SDL_bool status) {
+    if (status != SDL_TRUE) {
+        throw std::runtime_error("Operation returned unexpected false");
+    }
+}
+
+static void drawLineSimple(SDL_Renderer& renderer, int x1, int y1, int x2, int y2);
+
 int main(int argc, char *argv[]) {
     const int width = 640;
     const int height = 480;
+
+    // Enable antialiasing
+    check_sdl_bool(SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"));
 
     Window window(SDL_CreateWindow(
         "Hello World",
@@ -51,10 +63,36 @@ int main(int argc, char *argv[]) {
     // Draw line
     check_sdl_status(SDL_RenderDrawLine(renderer.get(), 0, 0, width - 1, height - 1));
 
+    // Draw line (Bresenham's algorithm)
+    drawLineSimple(*renderer, 0, height - 1, width - 1, 0);
+
     SDL_RenderPresent(renderer.get());
 
-    SDL_Delay(1000);
+    SDL_Delay(2000);
     SDL_Quit();
 
     return EXIT_SUCCESS;
+}
+
+// Draw line with no antialiasing etc.
+void drawLineSimple(SDL_Renderer& renderer, int x1, int y1, int x2, int y2) {
+    const int dx = x2 - x1;
+    const int dy = y2 - y1;
+
+    if (abs(dx) > abs(dy)) {
+        const int xMin = std::min(x1, x2);
+        const int xMax = std::max(x1, x2);
+        for (int x = xMin; x <= xMax; ++x) {
+            const int y = y1 + static_cast<int>(dy * (x - x1) / dx);
+            check_sdl_status(SDL_RenderDrawPoint(&renderer, x, y));
+        }
+    }
+    else {
+        const int yMin = std::min(y1, y2);
+        const int yMax = std::max(y1, y2);
+        for (int y = yMin; y <= yMax; ++y) {
+            const int x = x1 + static_cast<int>(dx * (y - y1) / dy);
+            check_sdl_status(SDL_RenderDrawPoint(&renderer, x, y));
+        }
+    }
 }
